4abf5a03fa9925a755ae843c3dafa876ceafe92a
Luke Pulverenti <luke.pulverenti@gmail.com>
2020-03-17 15:29:57 -0400

    fix api param

--- Emby.Kodi.SyncQueue/API/SyncAPI.cs
+++ Emby.Kodi.SyncQueue/API/SyncAPI.cs
@@ -9,9 +9,11 @@ namespace Emby.Kodi.SyncQueue.API
     [Route("/Emby.Kodi.SyncQueue/{UserID}/GetItems", "GET", Summary = "Gets Items for {UserID} from {UTC DATETIME} formatted as yyyy-MM-ddTHH:mm:ssZ using queryString LastUpdateDT")]
     public class GetLibraryItemsQuery : IReturn<SyncUpdateInfo>
     {
+        [ApiMember(Name = "UserID", Description = "User Id", IsRequired = true, DataType = "string", ParameterType = "path", Verb = "GET")]
+        public string UserID { get; set; }
+
         [ApiMember(Name = "LastUpdateDT", Description = "UTC DateTime of Last Update, Format yyyy-MM-ddTHH:mm:ssZ", IsRequired = false, DataType = "string", ParameterType = "query", Verb = "GET")]
         public string LastUpdateDT { get; set; }
-
     }
 
     public class SyncAPI : IService
